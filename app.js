f838241686fb7fdedc71b90c6702ec2d684e55ec
Freguin <frog14boy@gmail.com>
2016-08-27 23:42:51 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-var main = function(){
-$('.icon').click(function()
-{
-$('.menu').animate({
-left: "0px"
-}, 200);
+var main = function() {
+  $('.icon-menu').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200);
 
-$('body').animate({
-left:"300px"
-}, 200);
-});
+    $('body').animate({
+      left: "300px"
+    }, 200);
+  });
 };
-
 $(document).ready(main);
