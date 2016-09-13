ee1ea68b96c3585203b475a06bbd4886f70a2953
Freguin <frog14boy@gmail.com>
2016-09-12 21:12:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var main = function() {
- $('.drawfee-logo').delay(1000).fadeIn(1200).css("display","inline").addClass('show').delay(1000).fadeOut(1000).removeClass('show');
+ $('.drawfee-logo').fadeIn(2000).css("display","inline").addClass('show').delay(1000).fadeOut(1000).removeClass('show');
  // $('.main-body').delay(1500).addClass('show');
 };
 $(document).ready(main);
