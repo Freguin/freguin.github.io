cbb1139bb24cd32ed9a304296f2300ad7c0bc812
Freguin <frog14boy@gmail.com>
2016-09-13 18:08:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var main = function() {
  $('.our-logo').fadeIn(2800).css("display","inline").delay(1700).fadeOut(1000, function(){
-   // $('.main-body').delay(1500).addClass('show');
+   $('.main-body').slideDown(200);
  });
 };
 $(document).ready(main);
