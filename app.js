2de08b1f89f41a7c98a35baf3ed91e00387b21e6
Freguin <frog14boy@gmail.com>
2016-09-14 20:44:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,5 @@ var main = function() {
  $('.our-logo').fadeIn(2800).css("display","inline").delay(1700).fadeOut(1000, function(){
    $('.main-body').slideDown(200).css("display","block");
  });
- $('.arrow').click(function() {
-  $('.main-body').animate({
-   margin-bottom:-50vh;
-  },400);
-  $('.extra-info').animate({
-   margin-top:0vh;
-  },400)
- });
 };
 $(document).ready(main);
